6dab6b1dc678ad671119ea7884c134e4e33b8943
hmilly <milstowers@gmail.com>
2023-06-21 21:19:58 +0100

    adding fns for err, schema and extra routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,45 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
-// desc:   Auth user / set token
-// route:  POST   /api/users   Register a user
-// access: public
+// desc:   Register a user / set token
+// route:  POST   /api/users
+// access: Public
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
 });
 
-export { authUser };
+// desc:   Authenticate a user and get a token
+// route:  POST   /api/users/auth
+// access: Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+// desc:   Logout a user and clear cookie
+// route:  POST   /api/users/logout
+// access: Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+});
+
+// desc:   Get user profile
+// route:  GET   /api/users/profile
+// access: Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+
+// desc:   update user profile
+// route:  PUT   /api/users/profile
+// access: Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server ready"));
 
+app.use(notFound);
+app.use(errorHandler);
+
 //  post   /api/users           Register a user
+//  post   /api/users/auth      Authenticate a user and get a token
+//  post   /api/users/logout    Logout a user and clear cookie
+//  get    /api/users/profile   Get user profile
+//  put    /api/users/profile   Update profile
+
 app.listen(port, () => console.log(`listening on port: ${port}`));
