c587d49cf0a440f9dc35abcb368e53d3fb57bde9
hmilly <milstowers@gmail.com>
2023-06-27 11:56:37 +0100

    adding: routes, header, homescreen and toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <>
-    <h1>Mern Auth</h1>
+      <Header />
+      <ToastContainer />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
